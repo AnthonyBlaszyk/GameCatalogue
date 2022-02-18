d40145e41854236a4b47849a8c1954e3bef192ae
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-18 11:44:05 +0100

    Pagination rework

--- src/index.ts
+++ src/index.ts
@@ -26,14 +26,10 @@ app.get("/games", (req, response) => {
     const totalResult = JSON.parse(body);
 
     let page = 1;
-
-    if (
-      req.query.page === undefined ||
-      Number(req.query.page) <= 0 ||
-      isNaN(Number(req.query.page)) ||
-      Number(req.query.page) > Math.round(Number(totalResult.total) / 20)
-    ) {
+    if (req.query.page === undefined || Number(req.query.page) <= 0 || isNaN(Number(req.query.page))) {
       response.redirect("/games?page=1");
+    } else if (Number(req.query.page) > Math.round(Number(totalResult.total) / 20)) {
+      response.redirect("/games?page=4058");
     } else {
       page = Number(req.query.page);
 
@@ -63,14 +59,6 @@ app.get("/games/:slug", (req, response) => {
 
 // Games by plateform page
 app.get("/platform/:slug", (req, response) => {
-  let page = 1;
-
-  if (req.query.pages === undefined || Number(req.query.pages) <= 0 || isNaN(Number(req.query.pages))) {
-    page = 1;
-  } else {
-    page = Number(req.query.pages);
-  }
-
   request(`http://videogame-api.fly.dev/platforms/slug/${req.params.slug}`, (error, body) => {
     if (error) {
       throw error;
@@ -79,35 +67,69 @@ app.get("/platform/:slug", (req, response) => {
     const result = JSON.parse(body);
     const slug = req.params.slug;
 
-    request(`http://videogame-api.fly.dev/games/platforms/${result.id}?page=${page}`, (error, body) => {
+    request(`http://videogame-api.fly.dev/games/platforms/${result.id}`, (error, body) => {
       if (error) {
         throw error;
       }
 
-      const resultPlatform = JSON.parse(body);
+      const totalResult = JSON.parse(body); //8834
+      const total: number = totalResult.total;
 
-      response.render("gameByPlatform", { gamesByPlatform: resultPlatform, slug: slug, pages: page });
+      let page = 1;
+      if (req.query.pages === undefined || Number(req.query.pages) <= 0 || isNaN(Number(req.query.pages))) {
+        response.redirect(`/platform/${slug}/?pages=1`);
+      } else if (Number(req.query.pages) > total / 20 + 1) {
+        response.redirect(`/platform/${slug}/?pages=${Math.round(total / 20)}`);
+      } else {
+        page = Number(req.query.pages);
+
+        request(`http://videogame-api.fly.dev/games/platforms/${result.id}?page=${page}`, (error, body) => {
+          if (error) {
+            throw error;
+          }
+
+          const resultPlatform = JSON.parse(body);
+
+          response.render("gameByPlatform", {
+            gamesByPlatform: resultPlatform,
+            slug: slug,
+            pageNumber: page,
+            totalPages: Math.round(total / 20),
+          });
+        });
+      }
     });
   });
 });
 
 // Platform page
 app.get("/platform", (req, response) => {
-  let page = 1;
-
-  if (req.query.page === undefined || Number(req.query.page) <= 0 || isNaN(Number(req.query.page))) {
-    page = 1;
-  } else {
-    page = Number(req.query.page);
-  }
-
-  request(`http://videogame-api.fly.dev/platforms?page=${page}`, (error, body) => {
+  request("http://videogame-api.fly.dev/platforms", (error, body) => {
     if (error) {
       throw error;
     }
-    const result = JSON.parse(body);
 
-    response.render("platform", { platformsList: result.platforms, pageNumber: page });
+    const totalResult = JSON.parse(body);
+
+    let page = 1;
+
+    if (req.query.page === undefined || Number(req.query.page) <= 0 || isNaN(Number(req.query.page))) {
+      response.redirect("/platform?page=1");
+    } else if (Number(req.query.page) > Math.round(Number(totalResult.total) / 20) + 1) {
+      response.redirect("/platform?page=10");
+    } else {
+      page = Number(req.query.page);
+
+      request(`http://videogame-api.fly.dev/platforms?page=${page}`, (error, body) => {
+        if (error) {
+          throw error;
+        }
+
+        const result = JSON.parse(body);
+
+        response.render("platform", { platformsList: result.platforms, pageNumber: page });
+      });
+    }
   });
 });
 
